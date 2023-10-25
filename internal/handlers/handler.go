c25dd460f54adc524bd402659e9a9192b543abb8
Khomkrid.Ler <aofiee@aofiee.dev>
2023-10-25 19:25:16 +0700

    feat: implement handlers

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -45,20 +46,144 @@ func (hdl *HTTPHandler) HealthCheck(c *fiber.Ctx) error {
 
 // CreateTodo func
 func (hdl *HTTPHandler) CreateTodo(c *fiber.Ctx) error {
-	return nil
+	var request domain.TodoRequest
+	if err := c.BodyParser(&request); err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	if err := hdl.validator.ValidateStruct(request); err != nil {
+		msg := domain.ResponseBody{
+			Status: domain.BadRequest,
+		}
+		msg.Status.Message = []string{
+			err.Error(),
+		}
+		return c.Status(fiber.StatusBadRequest).JSON(msg)
+	}
+	response, err := hdl.srv.CreateTodo(request)
+	if err != nil {
+		logrus.Errorln(err)
+		msg := domain.ResponseBody{
+			Status: domain.InternalServerError,
+		}
+		msg.Status.Message = []string{
+			err.Error(),
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(msg)
+	}
+	return c.Status(fiber.StatusOK).JSON(domain.ResponseBody{Status: domain.Success, Data: response})
 }
 
 // UpdateTodo func
 func (hdl *HTTPHandler) UpdateTodo(c *fiber.Ctx) error {
-	return nil
+	var request domain.TodoRequest
+	if err := c.BodyParser(&request); err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	if err := hdl.validator.ValidateStruct(request); err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	if request.ID == nil {
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	response, err := hdl.srv.UpdateTodo(request)
+	if err != nil {
+		msg := domain.ResponseBody{
+			Status: domain.InternalServerError,
+		}
+		msg.Status.Message = []string{
+			err.Error(),
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(msg)
+	}
+	return c.Status(fiber.StatusOK).JSON(domain.ResponseBody{Status: domain.Success, Data: response})
 }
 
 // DeleteTodo func
 func (hdl *HTTPHandler) DeleteTodo(c *fiber.Ctx) error {
-	return nil
+	var (
+		uid uuid.UUID
+		err error
+	)
+	id := c.Params("id")
+	if id != "" {
+		uid, err = uuid.Parse(id)
+		if err != nil {
+			logrus.Errorln(err)
+			return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+		}
+	}
+
+	var request domain.TodoRequest
+	if err := c.BodyParser(&request); err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	if err := hdl.validator.ValidateStruct(request); err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+	request.ID = &uid
+	response, err := hdl.srv.DeleteTodo(request)
+	if err != nil {
+		msg := domain.ResponseBody{
+			Status: domain.InternalServerError,
+		}
+		msg.Status.Message = []string{
+			err.Error(),
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(msg)
+	}
+	return c.Status(fiber.StatusOK).JSON(domain.ResponseBody{Status: domain.Success, Data: response})
 }
 
 // GetTodo func
 func (hdl *HTTPHandler) GetTodo(c *fiber.Ctx) error {
-	return nil
+	var (
+		uid  uuid.UUID
+		err  error
+		data []domain.TodoResponse
+	)
+	condition := domain.QueryTodoRequest{}
+	err = c.QueryParser(&condition)
+	if err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+
+	err = hdl.validator.ValidateStruct(condition)
+	if err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+	}
+
+	id := c.Params("id")
+	if id != "" {
+		uid, err = uuid.Parse(id)
+		if err != nil {
+			logrus.Errorln(err)
+			return c.Status(fiber.StatusBadRequest).JSON(domain.ResponseBody{Status: domain.BadRequest})
+		}
+		condition.ID = &uid
+	}
+	result, err := hdl.srv.GetTodo(condition)
+	if err != nil {
+		logrus.Errorln(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(domain.ResponseBody{Status: domain.InternalServerError})
+	}
+	if result.Todos == nil {
+		data = make([]domain.TodoResponse, 0)
+	} else {
+		data = result.Todos
+	}
+
+	return c.Status(fiber.StatusOK).JSON(domain.ResponseBody{
+		Status:      domain.Success,
+		Data:        data,
+		CurrentPage: result.CurrentPage,
+		PerPage:     result.PerPage,
+		TotalItem:   result.TotalItem,
+	})
 }
