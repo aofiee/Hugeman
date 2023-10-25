bf6cefe35aaef51894d085f8ba7d43a7b35b060e
Khomkrid.Ler <aofiee@aofiee.dev>
2023-10-25 19:29:44 +0700

    feat: implement service

--- internal/services/service.go
+++ internal/services/service.go
@@ -19,20 +19,57 @@ func New(repo ports.Repository) *Service {
 
 // CreateTodo func
 func (s *Service) CreateTodo(request domain.TodoRequest) (*domain.TodoResponse, error) {
-	return nil, nil
+	return s.repo.CreateTodo(request)
 }
 
 // UpdateTodo func
 func (s *Service) UpdateTodo(request domain.TodoRequest) (*domain.TodoResponse, error) {
-	return nil, nil
+	return s.repo.UpdateTodo(request)
 }
 
 // DeleteTodo func
 func (s *Service) DeleteTodo(request domain.TodoRequest) (*domain.TodoResponse, error) {
-	return nil, nil
+	return s.repo.DeleteTodo(request)
 }
 
 // GetTodo func
 func (s *Service) GetTodo(condition domain.QueryTodoRequest) (*domain.TodoListResponse, error) {
-	return nil, nil
+	var (
+		page    int
+		perPage int
+		offset  int
+	)
+	if condition.Page != nil {
+		page = *condition.Page
+	} else {
+		page = 1
+		condition.Page = &page
+	}
+	if condition.Limit != nil {
+		perPage = *condition.Limit
+	} else {
+		perPage = 100
+		condition.Limit = &perPage
+	}
+	offset = (page - 1) * perPage
+	condition.Pagination = &domain.Pagination{
+		Limit:  perPage,
+		Offset: offset,
+	}
+	if condition.OrderBy != nil {
+		asc := true
+		if condition.Asc != nil {
+			asc = *condition.Asc
+		}
+		condition.SortMethod = &domain.SortMethod{
+			Asc:     asc,
+			OrderBy: *condition.OrderBy,
+		}
+	} else {
+		condition.SortMethod = &domain.SortMethod{
+			Asc:     true,
+			OrderBy: "ID",
+		}
+	}
+	return s.repo.GetTodo(condition)
 }
